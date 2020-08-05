a1db7cc5440495498d81debae061a64ee2ee5c3e
Dennys Kaluzny Pereira <64793570+protonnegativo@users.noreply.github.com>
2020-08-05 15:59:39 -0300

    Update exercicio_2_protonNegativo.sh

--- ShellScript/Exercicios/exercicio_2_protonNegativo.sh
+++ ShellScript/Exercicios/exercicio_2_protonNegativo.sh
@@ -4,4 +4,5 @@ echo "Qual site desejado?"
 read alvo
 ping -c2 $alvo > ping.txt
 echo "voce esta:" `pwd`
-echo "o resultado do ping foi:" `cat ping.txt`
+echo "o resultado do ping foi:" 
+cat ping.txt
