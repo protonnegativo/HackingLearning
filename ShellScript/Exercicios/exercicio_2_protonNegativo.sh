f1fc26f6f9e9cbe96b104a4215b7afb7d22496c7
Dennys Kaluzny Pereira <64793570+protonnegativo@users.noreply.github.com>
2020-08-05 16:16:18 -0300

    addBanner

--- ShellScript/Exercicios/exercicio_2_protonNegativo.sh
+++ ShellScript/Exercicios/exercicio_2_protonNegativo.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo -e "\033[0;30m  _____           _
+ |  __ \         | |
+ | |__) | __ ___ | |_ ___  _ __
+ |  ___/ '__/ _ \| __/ _ \| '_ \ 
+ | |   | | | (_) | || (_) | | | |
+ |_|   |_|  \___/ \__\___/|_| |_| \033[0m
+                           
+                     "
 echo "Bom dia usuario" `whoami`
 echo "Qual site desejado?"
 read alvo
